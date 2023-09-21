763af83dc1a9c9d824edd3faa22639e8ca2f6620
doutoramon <doutoramon@gmail.com>
2023-09-20 23:00:23 -0300

    solveMeFirst resolution

---
+++ solveMeFirst .ts
@@ -0,0 +1,11 @@
+//Complete the function solveMeFirst to compute the sum of two integers.
+
+function solveMeFirst(a: number,b: number) {
+  return a + b;
+}
+
+function main() {
+    let a = parseInt(readLine());
+    let b = parseInt(readLine());
+    console.log(solveMeFirst(a, b));
+}
